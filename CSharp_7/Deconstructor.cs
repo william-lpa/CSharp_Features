8f04c3bf2572f3c2ed96d1812d72df404a180a67
william-lpa <william-lpa@hotmail.com>
2017-07-21 11:51:25 -0300

    Deconstructor and return of Tuple.

---
+++ CSharp_7/Deconstructor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CSharp_7
+{
+    class Deconstructor //not Desconstructor
+    {
+        public Deconstructor()
+        {
+            var now = new Rectangle(2, 5, 44, 60);
+            Console.WriteLine($"Area:{rectangle1.Area}, Width:{rectangle1.X}, Height:{rectangle1.Y}");
+        }
+
+        public void BeforeDeconstructor()
+        {
+            var rectangle = new Rectangle(2, 5, 44, 60);
+            Console.WriteLine($"Area:{rectangle.Area}, X:{rectangle.X}, Y:{rectangle.Y}");
+        }
+        public void AfterDeconstructor()
+        {
+            var (x, y, rectangleArea) = new Rectangle(2, 5, 44, 60);
+            Console.WriteLine($"Area:{rectangleArea}, X:{x}, Y:{y}");
+        }
+    }
+
+    struct Rectangle
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public double Area { get; }
+
+        public Rectangle(int x, int y, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Area = Height * Width;
+        }
+        public void Deconstruct(out int x, out int y, out double area)
+        {
+            x = X;
+            y = Y;
+            area = Area;
+        }
+
+    }
+}
+
---
+++ CSharp_7/TupleReturn.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_7
+{
+    class TupleReturn
+    {
+        public TupleReturn()
+        {
+            InvokeOldTuple();
+            InvokeNewTuple();
+        }
+
+        public void InvokeNewTuple()
+        {
+            //(string firstName, string lastName) = FullNameAfteTupleReturn();
+            //string (firstName, lastName) = FullNameAfteTupleReturn();
+            var (firstName, lastName) = LookUpAfteTupleReturn(0);
+            Console.WriteLine($"{firstName} {lastName}");
+        }
+        public void InvokeOldTuple()
+        {
+            var name = LookUpBeforeTupleReturn(0); //ValueTuple<string, string>
+            Console.WriteLine($"{name.Item1} {name.Item2}");
+        }
+
+
+        public Tuple<string, string> LookUpBeforeTupleReturn(long id) // tuple return type
+        {
+            return new Tuple<string, string>("Leonard", "William"); // tuple literal
+        }
+
+        public (string, string) LookUpAfteTupleReturn(long id) // tuple return type. You can add identifiers to them
+        {
+            return ( "William", "Pegler"); // tuple literal
+        }
+
+
+    }
+}
