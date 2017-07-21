00a30330b48e474b923b5d34efdfe322c192ea79
william-lpa <william-lpa@hotmail.com>
2017-07-21 14:48:16 -0300

    Out parameter  and nested function

---
+++ CSharp_7/NestedFunctions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_7
+{
+    class NestedFunctions
+    {
+        public NestedFunctions()
+        {
+            GetMaxBeforeDateNestedFunctions(new List<DateTime>() { });
+
+        }
+        public DateTime GetMaxBeforeDateNestedFunctions(IList<DateTime> values)
+        {
+            Func<DateTime, DateTime, DateTime> ReturnBiggerDate = (leftDate, rightDate) =>
+             {
+                 return (leftDate > rightDate) ? leftDate : rightDate;
+             };
+
+            var biggestDate = values.First();
+
+            foreach (var date in values)
+                biggestDate = ReturnBiggerDate(biggestDate, date);
+
+            return biggestDate;
+        }
+
+        public DateTime GetMaxAfterDateNestedFunctions(IList<DateTime> values)
+        {
+            DateTime ReturnBiggerDate(DateTime leftDate, DateTime rightDate)
+            {
+                return (leftDate > rightDate) ? leftDate : rightDate;
+            };
+
+            var biggestDate = values.First();
+
+            foreach (var date in values)
+                biggestDate = ReturnBiggerDate(biggestDate, date);
+
+            return biggestDate;
+        }
+    }
+}
+}
--- CSharp_7/OutSelfDeclaring.cs
+++ CSharp_7/OutSelfDeclaring.cs
@@ -6,106 +6,27 @@ namespace CSharp7
 {
     public class OutSelfDeclaring
     {
-        static void Main(string[] args)
+        public OutSelfDeclaring()
         {
-
+            BeforeOutSelfDeclaring();
+            AfterOutSelfDeclaring();
         }
 
-        public (string A, string B, string C) LookupName(long id) // tuple return type
+        private void BeforeOutSelfDeclaring()
         {
+            var convertedNumber = 0;
 
-            return ("", "", ""); // tuple literal
+            if (int.TryParse("123", out convertedNumber))
+            {
+                Console.WriteLine(convertedNumber);
+            }
         }
-
-        #region Before C#7
-        public void PrintCoordinates(Point p)
+        private void AfterOutSelfDeclaring()
         {
-            int x, y; // have to "predeclare"
-            p.GetCoordinatesIncresedByOne(out x, out y);
-            Console.WriteLine($"({x}, {y})");
+            if (int.TryParse("123", out var convertedNumber)) //you can even ignore de value with a _
+            {
+                Console.WriteLine(convertedNumber);
+            }
         }
-        #endregion
-
-        #region With C#7
-        public void PrintCoordinatesNewVersion(Point p)
-        {
-            var g = LookupName(4);
-            p.GetCoordinatesIncresedByOne(out x, out y);
-            Console.WriteLine($"({x}, {y})");
-        }
-        #endregion
-
-        public void PrintStars(Point p)
-        {
-            string s = "s";
-
-            if (int.TryParse(s, out var i)) { Console.WriteLine(new string('*', i)); }
-            else { Console.WriteLine("Cloudy - no stars tonight!"); }
-
-
-            var (x, y) = p; // deconstructing declaration
-        }
-
-        public void TesteTupla()
-        {
-            //var tuple = Tuple.Create("foo", 42);
-            //var (name, value) = tuple;
-
-            var file = new FileInfo("c:/teste.txt");
-            var (name, extension, length) = file;
-                
-
-        }
-
-    }
-
-    public static class ExtensionDesconstructor
-    {
-        public static void Deconstruct<T1, T2>(this Tuple<T1, T2> tuple, out T1 item1, out T2 item2)
-        {
-            item1 = tuple.Item1;
-            item2 = tuple.Item2;
-        }
-        public static void Deconstruct(this FileInfo reader, out long length, out string name, out string extension)
-        {
-            length = reader.Length;
-            name = reader.Name;
-            extension = reader.Extension;
-        }
-
-    }
-
-    public class Point
-    {
-        private int x;
-        private int y;
-        public int X
-        {
-            get { return x; }
-            set { x = value; }
-        }
-        public int Y
-        {
-            get { return y; }
-            set { y = value; }
-        }
-        internal Point(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-        public void Deconstruct(out int x, out int y)
-        {
-            x = X;
-            y = Y;
-        }
-
-        public Point GetCoordinatesIncresedByOne(out int x, out int y)
-        {
-            x = X + 1;
-            y = Y + 1;
-            return new Point(x, y);
-        }
-
     }
 }
