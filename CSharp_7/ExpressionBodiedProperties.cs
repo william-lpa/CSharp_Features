7b3bd0e41997c9f363ad7ebfe937185d4a23c569
william-lpa <william-lpa@hotmail.com>
2017-07-25 16:09:47 -0300

    Ref variables and expressionBodies

---
+++ CSharp_7/ExpressionBodiedProperties.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CSharp_7
+{
+    class ExpressionBodiedProperties
+    {
+        private string firstName;
+        private FileStream serialize;
+
+        public ExpressionBodiedProperties()
+        {
+            OldFirstName = "William";
+            Console.WriteLine(OldFirstName);
+            NewFirstName = "William";
+            Console.WriteLine(NewFirstName);
+            serialize = new FileStream(@"C:\Teste\package.json",FileMode.Open);
+        }
+
+        public ExpressionBodiedProperties(string firstName) => NewFirstName = firstName;
+
+        public string OldFirstName
+        {
+            get { return firstName; }
+            set { ValidadeName(value); }
+        }
+        public string NewFirstName
+        {
+            get => firstName;
+            set => ValidadeName(value);
+        }
+
+        private void ValidadeName(string value)
+        {
+            if (!string.IsNullOrEmpty(value) && value.Length > 3)
+            {
+                firstName = value;
+            }
+        }
+
+        ~ExpressionBodiedProperties() => serialize?.Dispose(); //elvis
+
+    }
+}
---
+++ CSharp_7/RefLocals.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_7
+{
+    class RefLocals
+    {
+        public RefLocals()
+        {
+            ProcessAuthenticatedUsers();
+            var s = new Shape();
+            var rect = new Rectangle(0, 0, 10, 20);
+            s.Size = rect;
+        }
+
+        private void ProcessAuthenticatedUsers()
+        {
+            var users = new List<UserAuthentication>();
+            for (int i = 0; i < 20; i++)
+            {
+                var user = new UserAuthentication("userName");
+                ref UserAuthentication userRef = ref user;
+                userRef.UserName = "userName" + i;
+                users.Add(userRef);
+            }
+        }
+    }
+
+    class Shape
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        Rectangle m_Size;
+        public ref Rectangle Size { get { return ref m_Size; } }
+
+        public ref int SetAndGetX(ref int x)
+        {
+            return ref x;
+        }
+
+    }
+    struct UserAuthentication
+    {
+        public string UserName { get; set; }
+        public Guid Token { get; set; }
+
+        public UserAuthentication(string userName)
+        {
+            UserName = userName;
+            Token = Guid.NewGuid();
+        }
+    }
+
+}
